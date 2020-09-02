e9bcc093fefa03a522de47c1981d7992e89a00dc
Rohan-Dawar <68876087+Rohan-Dawar@users.noreply.github.com>
2020-09-02 15:32:55 -0400

    Update game.py

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from io import BytesIO
 from check_ag_lists import check_name
 from prawKeys import reddit
 import requests, time, secrets, unidecode, jsons
-en_core_web_sm = __import__('spacy.en-core-web-sm')
+en_core_web_sm = __import__('spacy.lang.en-core-web-sm')
 
 # Flask Init
 app = Flask(__name__)
