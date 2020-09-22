682478f8aac443c274e8a8a9331b0ddc4c6f1617
Rohan-Dawar <68876087+Rohan-Dawar@users.noreply.github.com>
2020-09-22 10:35:40 -0400

    Delete prawKeysOG.py

--- prawKeysOG.py
+++
@@ -1,8 +0,0 @@
-import praw
-
-reddit = praw.Reddit(
-    client_id = '6kOVwD2_1wUXsg',
-    client_secret = 'w73we8IVCOuanyAunemR3xqevw4',
-    username = 'seabufixture',
-    password = '!7Aloyarc_SB',
-    user_agent = 'prawtutorialv1')
